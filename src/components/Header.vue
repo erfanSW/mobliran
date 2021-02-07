8f9121d7b8dce1f48d13596fc2df592686a38c44
erfan.chegini <iirfansardivand@gmail.com>
2021-02-07 11:21:20 +0330

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="header">
+    <ul>
+      <li class="header-title">
+        فروشگاه مبلیران
+      </li>
+    </ul>
+    <ul class="header-mt">
+      <li>
+        محصولات ↓
+      </li>
+      <li>
+        اتاق ها
+      </li>
+      <li>
+        طراحی اختصاصی
+      </li>
+    </ul>
+    <ul class="header-mt">
+      <li>
+        <img src="../assets/Profile.svg" />
+      </li>
+      <li>
+        <img src="../assets/Search.svg" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  font-size: 15px;
+}
+
+li:not(:first-child) {
+  margin-right: 31px;
+}
+
+.logo {
+  width: 39px;
+  height: 34px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-around;
+  top: 0;
+  right: 0;
+  left: 0;
+  background-color: ;
+}
+
+.header-title {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.header-mt {
+  margin-top: 30px;
+}
+
+</style>
