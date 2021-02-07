82bedd4bcd2df57f60990168dc43771b60124605
erfan.chegini <iirfansardivand@gmail.com>
2021-02-08 02:25:07 +0330

    last commit before merge request . feature : header added and prepared for children binding

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+body {
+  margin: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   direction: rtl;
+  box-sizing: border-box;
 }
 
 @font-face {
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="header">
-    <ul>
-      <li class="header-title">
-        فروشگاه مبلیران
-      </li>
-    </ul>
-    <ul class="header-mt">
-      <li>
-        محصولات ↓
-      </li>
-      <li>
-        اتاق ها
-      </li>
-      <li>
-        طراحی اختصاصی
-      </li>
-    </ul>
-    <ul class="header-mt">
-      <li>
-        <img src="../assets/Profile.svg" />
-      </li>
-      <li>
-        <img src="../assets/Search.svg" />
-      </li>
-    </ul>
+  <div>
+    <div class="header">
+      <ul>
+        <li class="header-title">
+          فروشگاه مبلیران
+        </li>
+      </ul>
+      <ul class="header-mt">
+        <li>
+          محصولات ↓
+        </li>
+        <li>
+          اتاق ها
+        </li>
+        <li>
+          طراحی اختصاصی
+        </li>
+      </ul>
+      <ul class="header-mt">
+        <li>
+          <img src="../assets/Profile.svg" />
+        </li>
+        <li>
+          <img src="../assets/Search.svg" />
+        </li>
+      </ul>
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style css-scoped>
 ul {
   list-style-type: none;
   padding: 0;
@@ -55,21 +55,25 @@ li:not(:first-child) {
 }
 
 .header {
+  color: white;
   display: flex;
   justify-content: space-around;
-  top: 0;
+  position: fixed;
+  top: 30px;
   right: 0;
   left: 0;
-  background-color: ;
+}
+
+.header:after {
+  color: black;
 }
 
 .header-title {
   font-size: 30px;
-  font-weight: bold;
+  font-weight: 400;
 }
 
 .header-mt {
   margin-top: 30px;
 }
-
 </style>
